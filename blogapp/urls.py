1d039f6d7aa47752b3eb96676e4029b925ff1c24
hafiza2601 <hafizachhipa@gmail.com>
2021-01-12 02:35:57 +0530

    updated url and setting

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
+from myblog import settings
 
 
 urlpatterns = [
--- myblog/settings.py
+++ myblog/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
-#MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/'
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
